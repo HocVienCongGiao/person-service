f1a68a6c4ead8974a03b41584ea1c578bd7bda6f
Henry <minhhieu060799@gmail.com>
2022-03-08 22:41:23 +0700

    [add]: delete person by id

--- person-lambda/person/src/delete_person.rs
+++ person-lambda/person/src/delete_person.rs
@@ -1,20 +1,22 @@
-use lambda_http::{Body, Request, RequestExt, Response};
-use lambda_http::http::StatusCode;
-use uuid::Uuid;
 use crate::build_response;
 use crate::parse_request::from_request_to_id;
+use domain::usecases::UsecaseError;
+use lambda_http::http::StatusCode;
+use lambda_http::{Body, Request, RequestExt, Response};
+use uuid::Uuid;
 
 pub async fn execute(request: Request) -> Response<Body> {
     println!("Handle delete method");
-    let mut status_code= StatusCode::INTERNAL_SERVER_ERROR;
+    let mut status_code = StatusCode::INTERNAL_SERVER_ERROR;
     let result = match from_request_to_id(&request) {
         Some(id) => {
             status_code = StatusCode::NO_CONTENT;
             controller::delete_one_person_by_id(id).await
-        },
+        }
         _ => {
             status_code = StatusCode::NOT_FOUND;
-            None
+            // UsecaseError::ResourceNotFound
+            Ok(())
         }
     };
     // TODO: refactor
